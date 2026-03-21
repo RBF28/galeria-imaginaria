6301119434c4eb2546641231730510c3cee1747a
RBF28 <RBFilho2005@protonmail.com>
2026-03-21 16:13:06 -0300

    Add files via upload

---
+++ hqs/hqs.js
@@ -0,0 +1,27 @@
+// hqs.js - Versão 5.0 (Integração com Fliptru)
+hqs.push({
+    nome: "Cartunia! Em tirinhas (2025)",
+    capa: "https://files.catbox.moe/dep039.png",
+    tipo: "hq",
+    sinopse: "Uma coleção de tirinhas curtas e hilárias sobre o cotidiano em Cartunia. Perfeito para uma leitura rápida!",
+    link: "https://fliptru.com.br/comic/cartunia-em-tirinhas", 
+    novo: ""
+});
+
+hqs.push({
+    nome: "Cartunia! (2025)",
+    capa: "https://files.catbox.moe/vkxuil.png",
+    tipo: "hq",
+    sinopse: "A obra original. Acompanhe a grande aventura que apresenta o mundo e as personagens de Cartunia.",
+    link: "https://fliptru.com.br/comic/cartunia",
+    novo: "RENA"
+});
+
+hqs.push({
+    nome: "Cartuneiros! The Crazy Jornadas (2026)",
+    capa: "https://files.catbox.moe/akllrb.png",
+    tipo: "hq",
+    sinopse: "A jornada mais insana de sempre! Os Cartuneiros enfrentam desafios que desafiam a lógica e o bom senso.",
+    link: "https://fliptru.com.br/comic/cartuneiros-the-crazy-jornadas",
+    novo: "RENA"
+});
