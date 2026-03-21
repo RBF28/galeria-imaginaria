eb433ddd13720b4a5e8240461e9506f5510729c6
RBF28 <RBFilho2005@protonmail.com>
2026-03-21 16:29:20 -0300

    Update hqs.js

--- hqs/hqs.js
+++ hqs/hqs.js
@@ -3,25 +3,25 @@ hqs.push({
     nome: "Cartunia! Em tirinhas (2025)",
     capa: "https://files.catbox.moe/dep039.png",
     tipo: "hq",
-    sinopse: "Uma coleção de tirinhas curtas e hilárias sobre o cotidiano em Cartunia. Perfeito para uma leitura rápida!",
+    sinopse: "O mundo maluco de Cartunia… agora em tirinhas! Em Cartunia, Bluper e Lolly tentam manter o riso vivo enquanto encaram vilões que querem apagar a graça do mundo. Entre o caos e a comédia, eles descobrem que proteger a imaginação é o maior desafio de todos.",
     link: "https://fliptru.com.br/comic/cartunia-em-tirinhas", 
-    novo: ""
+    novo: "RENA"
 });
 
 hqs.push({
     nome: "Cartunia! (2025)",
     capa: "https://files.catbox.moe/vkxuil.png",
     tipo: "hq",
-    sinopse: "A obra original. Acompanhe a grande aventura que apresenta o mundo e as personagens de Cartunia.",
+    sinopse: "Tudo começa em Salvador, quando o impossível passa a rir e a realidade começa a se distorcer. Belto Santos e Araun Brito descobrem o Mady, uma energia que nasce do humor de cada usuário. Mas o caos cartunesco não se limita ao Brasil. Ele se espalha pelo mundo, atravessando continentes… até alcançar forças muito maiores escondidas no Norte da África.",
     link: "https://fliptru.com.br/comic/cartunia",
-    novo: "RENA"
+    novo: ""
 });
 
 hqs.push({
     nome: "Cartuneiros! The Crazy Jornadas (2026)",
     capa: "https://files.catbox.moe/akllrb.png",
     tipo: "hq",
-    sinopse: "A jornada mais insana de sempre! Os Cartuneiros enfrentam desafios que desafiam a lógica e o bom senso.",
+    sinopse: "Em Cartunia, um mundo onde a imaginação não tem limites, Bluper Dooble e Lolly Fluff vivem aventuras malucas cheias de humor e caos cartunesco. Entre coisas exageradas, desafios absurdos e vilões excêntricos, a única regra é se divertir!",
     link: "https://fliptru.com.br/comic/cartuneiros-the-crazy-jornadas",
     novo: "RENA"
 });
