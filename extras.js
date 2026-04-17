1e6e0b8faa5547289ec230f990be0e2f7a39db7e
RBF28 <RBFilho2005@protonmail.com>
2026-04-17 17:29:16 -0300

    Delete extras.js

--- extras.js
+++
@@ -1,5 +0,0 @@
-// extras.js - Versão 3.5
-// Caso adicione episódios extras no futuro, use:
-// extras.push({ nome: "Especial 01", link: "https://files.catbox.moe/link.mp4" });
-
-const capaExtras = "https://files.catbox.moe/jj59ob.png";
