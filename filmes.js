82a11b7e457f338061ad79cd07ae1d24815f0239
RBF28 <RBFilho2005@protonmail.com>
2026-04-17 17:29:28 -0300

    Delete filmes.js

--- filmes.js
+++
@@ -1,9 +0,0 @@
-// filmes.js - Versão 5.0
-filmes.push({
-    nome: "Knight of Nasa: A Revanche (2021)",
-    capa: "https://files.catbox.moe/ivppkz.png",
-    tipo: "filme",
-    link: "https://files.catbox.moe/zopirs.mp4",
-    novo: "RENA",
-    aviso: { texto: "️ Classificação: +14. Pode conter violência leve e temas mais intensos." }
-});
