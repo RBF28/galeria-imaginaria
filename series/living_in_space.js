5bffaa6df7a5f35a25541f714b5f11c6fbf619a6
RBF28 <RBFilho2005@protonmail.com>
2026-04-17 17:33:28 -0300

    Add files via upload

---
+++ series/living_in_space.js
@@ -0,0 +1,33 @@
+// Living_in_space.js - Versão 3.5 (Dados Puros)
+series.push({
+  nome: "Living in Space (2023)",
+  capa: "https://files.catbox.moe/2yw2xg.png",
+  tipo: "serie",
+  aviso: {
+    ativo: true,
+    texto: "⚠️ Aviso: Esta série é +16. Contém violência e sangue alienígena."
+  },
+  temporadas: [
+    {
+      nome: "Temporada 1",
+      episodios: [
+        { nome: "Cap 01", link: "https://files.catbox.moe/40gqfg.mp4", novo: false },
+        { nome: "Cap 02", link: "https://files.catbox.moe/6ytz9q.mp4", novo: false },
+        { nome: "Cap 03", link: "https://files.catbox.moe/9vuglb.mp4", novo: false },
+        { nome: "Cap 04", link: "https://files.catbox.moe/vs3tpa.mp4", novo: false },
+        { nome: "Cap 05", link: "https://files.catbox.moe/se1p0w.mp4", novo: false },
+        { nome: "Cap 06", link: "https://files.catbox.moe/ksqadl.mp4", novo: false },
+        { nome: "Cap 07", link: "https://files.catbox.moe/biy9oy.mp4", novo: false },
+        { nome: "Cap 08", link: "https://files.catbox.moe/o6gu5h.mp4", novo: false }
+      ]
+    },
+    {
+      nome: "Temporada 2",
+      episodios: [
+        { nome: "Cap 09", link: "https://files.catbox.moe/uvm7b5.mp4", novo: false },
+        { nome: "Cap 10", link: "https://files.catbox.moe/819fwn.mp4", novo: false },
+        { nome: "Cap 11", link: "https://files.catbox.moe/021qk9.mp4", novo: "RENA" }
+      ]
+    }
+  ]
+});
